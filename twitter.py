65af6144cd3d962078aea44ed69ac6af8caf4ad0
Rx-11 <110684815+Rx-11@users.noreply.github.com>
2023-10-25 01:54:53 +0530

    Update twitter.py

--- twitter.py
+++ twitter.py
@@ -20,8 +20,58 @@ client = tweepy.Client(
 )
 
 # Load tweets from GitHub Secrets
-json_content = os.getenv("TWEET_JSON")
-print(json_content)
+import json
+import os
+
+# Read the tips from the JSON file
+json_file = "x.json"
+with open(json_file, "r") as file:
+    data = json.load(file)
+    tips = data["millionaire_mindset_tips"]
+
+# Debugging output
+print("Tips:", tips)
+
+# Read and Update Last Tweeted Index
+last_tweeted_index_file = "last_tweeted_tip.txt"
+
+# Read the index of the last tweeted tip
+last_tweeted_index = 0
+if os.path.isfile(last_tweeted_index_file):
+    with open(last_tweeted_index_file, "r") as index_file:
+        last_tweeted_index = int(index_file.read().strip())
+
+# Debugging output
+print("Last tweeted index:", last_tweeted_index)
+
+# Determine the index of the next tip to tweet
+next_tip_index = last_tweeted_index + 1
+if next_tip_index >= len(tips):
+    next_tip_index = 0
+
+# Debugging output
+print("Next tip index:", next_tip_index)
+
+# Store the index of the next tip to tweet
+with open(last_tweeted_index_file, "w") as index_file:
+    index_file.write(str(next_tip_index))
+
+client = tweepy.Client(
+    consumer_key=consumer_key,
+    consumer_secret=consumer_secret,
+    access_token=access_token,
+    access_token_secret=access_token_secret,
+)
+
+tweet = tips[next_tip_index]
+
+ try:
+              client.create_tweet(text=tweet)
+              print(f"Tweeted: {tweet}")
+except Exception as e:
+               print(f"Error posting tweet: {e}")
+          
+          
 # tweets = json.loads(json_content).get("millionaire_mindset_tips", [])
 
 # # Function to save the updated JSON content to GitHub Secrets
