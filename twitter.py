82da05ae143beb8e69a2e083e57e6481877466ac
Rx-11 <110684815+Rx-11@users.noreply.github.com>
2023-10-24 22:28:11 +0530

    Update twitter.py

--- twitter.py
+++ twitter.py
@@ -22,20 +22,20 @@ client = tweepy.Client(
 # Load tweets from GitHub Secrets
 json_content = os.getenv("TWEET_JSON")
 print(json_content)
-tweets = json.loads(json_content).get("millionaire_mindset_tips", [])
+# tweets = json.loads(json_content).get("millionaire_mindset_tips", [])
 
-# Function to save the updated JSON content to GitHub Secrets
-def save_tweets_to_secrets(new_tweets):
-    data = {"millionaire_mindset_tips": new_tweets}
-    os.environ["TWEET_JSON"] = json.dumps(data)
+# # Function to save the updated JSON content to GitHub Secrets
+# def save_tweets_to_secrets(new_tweets):
+#     data = {"millionaire_mindset_tips": new_tweets}
+#     os.environ["TWEET_JSON"] = json.dumps(data)
 
-# Main loop to post tweets at regular intervals
-tweet = tweets[0]
+# # Main loop to post tweets at regular intervals
+# tweet = tweets[0]
 
-try:
-    client.create_tweet(text=tweet)
-    print(f"Tweeted: {tweet}")
-    tweets.remove(tweet)  # Remove the posted tweet
-    save_tweets_to_secrets(tweets)  # Update the JSON content in GitHub Secrets
-except Exception as e:
-    print(f"Error posting tweet: {e}")
+# try:
+#     client.create_tweet(text=tweet)
+#     print(f"Tweeted: {tweet}")
+#     tweets.remove(tweet)  # Remove the posted tweet
+#     save_tweets_to_secrets(tweets)  # Update the JSON content in GitHub Secrets
+# except Exception as e:
+#     print(f"Error posting tweet: {e}")
